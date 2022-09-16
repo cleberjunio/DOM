88df35e3bdc7a2e7c2a210e51874e396777cbef1
cleberjunio <juninhosete@yahoo.com.br>
2022-09-16 20:23:18 -0300

    Documentação do Código

--- manipulando_DOM/listaDeTarefas.js
+++ manipulando_DOM/listaDeTarefas.js
@@ -3,13 +3,15 @@ const novaTarefa = document.querySelector('[data-form-button]');
 const criarTarefa = (evento) => {
     evento.preventDefault()
 
-    const input = document.querySelector('[data-form-input]');
+    const input = document.querySelector('[data-form-input]'); //seleciona o input
     const valor = input.value;
 
 
-    const tarefa = document.querySelector('[data-task]');
+    const tarefa = document.querySelector('[data-task]'); //seleciona o li
     const conteudo = `<p class = "content">${valor}</p>`
+        //captura o valor digitado no input
     tarefa.innerHTML = conteudo;
+    //captura o valor digitado no input
     input.value = ""; //limpa o input
 
 }
